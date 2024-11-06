7f37e78599127aed59e2f807d55d892c787abbe1
kabileshcyfotok <kabileshkumar.s@cyfotok.com>
2024-11-06 11:51:48 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,12 @@
+# Mathematical operation
+a=int(input("Enter a number:"))
+b=int(input("ENter a number:"))
+add=a+b 
+sub=a-b             
+mul=a*b
+div=a/b
+mod=a%b 
+print("Addition:",add)
+print("Subtraction:",sub)
+print("modulas",mod)                                                                                                                                                                    
+print("modulas",mod)                                                                                                                                                                    
